21083433a74c3edade359946b660d560c97e06f3
Deniz <eme33@cam.ac.uk>
2026-01-23 15:12:14 +0000

    fixed issues

--- test_1D.py
+++ test_1D.py
@@ -10,9 +10,9 @@ def test_rivers_with_station():
     """test rivers_with_station returns unique river names"""
 
     stations = [ #fake station names
-        MonitoringStation("id1", "m1", "s1", (0, 0), "River Cam", "Town1"),
-        MonitoringStation("id2", "m2", "s2", (0, 0), "River Cam", "Town2"),
-        MonitoringStation("id3", "m3", "s3", (0, 0), "River Thames", "Town3"),]
+        MonitoringStation("id1", "m1", "s1", (0, 0), (0, 1), "River Cam", "Town1"),
+        MonitoringStation("id2", "m2", "s2", (0, 0), (0, 1), "River Cam", "Town2"),
+        MonitoringStation("id3", "m3", "s3", (0, 0), (0, 1),"River Thames", "Town3"),]
 
     rivers = rivers_with_station(stations)
     assert isinstance(rivers, set)
@@ -22,9 +22,9 @@ def test_rivers_with_station():
 def test_stations_by_river():
     """test stations_by_river groups stations correctly"""
 
-    s1 = MonitoringStation("id1", "m1", "Cambridge", (0, 0), "River Cam", "Cambridge")
-    s2 = MonitoringStation("id2", "m2", "Jesus Lock", (0, 0), "River Cam", "Cambridge") #i miss rowing on the cam :/
-    s3 = MonitoringStation("id3", "m3", "Oxford", (0, 0), "River Thames", "Oxford")
+    s1 = MonitoringStation("id1", "m1", "Cambridge", (0, 0),(0, 1), "River Cam", "Cambridge")
+    s2 = MonitoringStation("id2", "m2", "Jesus Lock", (0, 0),(0, 1), "River Cam", "Cambridge") #i miss rowing on the cam :/
+    s3 = MonitoringStation("id3", "m3", "Oxford", (0, 0),(0, 1), "River Thames", "Oxford")
     stations = [s1, s2, s3]
     rivers_dict = stations_by_river(stations)
     assert isinstance(rivers_dict, dict)
