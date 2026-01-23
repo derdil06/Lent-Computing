6a528cad9f02087963093320186127b88e8b2b53
Deniz <eme33@cam.ac.uk>
2026-01-23 13:06:11 +0000

    edited geo.py so i can use it in 1C

---
+++ Task1C.py
@@ -0,0 +1,19 @@
+from floodsystem.stationdata import build_station_list
+from floodsystem.geo import stations_within_radius
+
+
+def run():
+    """print stations within 10 km of Cambridge city centre."""
+    stations = build_station_list()
+    cambridge_centre = (52.2053, 0.1218)
+
+    nearby_stations = stations_within_radius(stations, cambridge_centre, 10)
+
+    station_names = sorted(station.name for station in nearby_stations)
+    print(station_names)
+
+
+if __name__ == "__main__":
+    """Run Task 1C."""
+    print("*** Task 1C: CUED Part IA Flood Warning System ***")
+    run()
--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,16 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+from haversine import haversine
+
+
+def stations_within_radius(stations, centre, r):
+    """return a list of stations within r of centre."""
+    stations_in_radius = []
+
+    for station in stations:
+        dist = haversine(centre, station.coord)
+        if dist <= r:
+            stations_in_radius.append(station)
+
+    return stations_in_radius
