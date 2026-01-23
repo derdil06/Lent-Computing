84f2166edc0f8449554e7e6d8819484a04cd3488
Deniz <eme33@cam.ac.uk>
2026-01-23 14:28:41 +0000

    edited geo.py so i can do 1D

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -20,3 +20,29 @@ def stations_within_radius(stations, centre, r):
             stations_in_radius.append(station)
 
     return stations_in_radius
+
+def rivers_with_station(stations):
+    """returns a set of river names that have at least one monitoring station on them"""
+
+    rivers = set()
+
+    for station in stations:
+        rivers.add(station.river)
+
+    return rivers
+
+
+def stations_by_river(stations):
+    """return a dictionary mapping river names to a list of stations on that river"""
+
+    rivers_dict = {}
+
+    for station in stations:
+        river = station.river
+
+        if river not in rivers_dict:
+            rivers_dict[river] = []
+
+        rivers_dict[river].append(station)
+
+    return rivers_dict
