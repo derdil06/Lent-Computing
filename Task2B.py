b2375b1b2b31c7c2d681002342d619081bfbbf37
Deniz <eme33@cam.ac.uk>
2026-01-26 16:35:35 +0000

    seems good

--- Task2B.py
+++ Task2B.py
@@ -7,7 +7,7 @@ def run():
     stations = build_station_list()
     update_water_levels(stations)
 
-    tol = 0.8
+    tol = 0.8 #this is quite low, returns a lot of stations, might be worth putting higher (or might just be a rainy day)
     stations_over = stations_level_over_threshold(stations, tol)
 
     for station, rel_level in stations_over:
