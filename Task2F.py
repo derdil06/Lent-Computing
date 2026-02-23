5c1e497d6ec2f8ee34ee76e60896cd4ee27750af
yg483 <yg483@cam.ac.uk>
2026-02-23 15:08:48 +0000

    right version for task 2F

--- Task2F.py
+++ Task2F.py
@@ -1,13 +1,44 @@
-import numpy as np
 import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import timedelta
+from floodsystem.station import polyfit
+from floodsystem.station import plot_water_level_with_fit
+from floodsystem.datafetcher import fetch_measure_levels
+from floodsystem.stationdata import build_station_list, update_water_levels
 
-def polyfit(dates, levels, p):
+def run():
+    stations = build_station_list()
+    update_water_levels(stations)
 
-    x = matplotlib.dates.date2num(dates)
-    d0 = x[0]
-    x_shifted = x - d0
+    station = None
+    for s in stations:
+        if s.latest_level is not None:
+            station = s
+            break
 
-    p_coeff = np.polyfit(x_shifted, levels, p)
-    poly = np.poly1d(p_coeff)
+    if station is None:
+        print('No station with data found.')
+        return
+    
+    dates, levels = fetch_measure_levels(station.measure_id, dt=timedelta(days=2))
 
-    return poly, d0
+    degree = 4
+
+    poly, d0 = polyfit(dates, levels, degree)
+    plt.plot(dates, levels, '.', label="Actual")
+    x_float = matplotlib.dates.date2num(dates) - d0
+    y_fit = poly(x_float)
+    plt.plot(dates, y_fit, '-', label=f"fit (deg={degree})")
+
+    plt.xlabel('Date')
+    plt.ylabel('Water level (m)')
+    plt.title(station.name)
+    plt.xticks(rotation=45)
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+
+if __name__ == "__main__":
+    run()
+
